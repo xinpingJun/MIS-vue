96d334c8e20e884223a1aa0137844fee74175a73
xpcss <903617610@qq.com>
2018-12-08 13:59:29 +0800

    00

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,12 @@ module.exports = {
     ],
 
   },
-   
+
+  baseUrl: './',
+  outputDir: undefined,
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
 }
